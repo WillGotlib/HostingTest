c41033a50d4faa4963eb82107027929d513b193c
WillGotlib <37600983+WillGotlib@users.noreply.github.com>
2023-11-12 18:20:41 -0500

    added admin panel stuff

--- runtimer/round/admin.py
+++ runtimer/round/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Movie, Round, RoundGuess, ScoringScheme
 
 # Register your models here.
+
+@admin.register(Movie)
+class Movie(admin.ModelAdmin):
+    pass
+
+class MoviesInline(admin.TabularInline):
+    model = Round.movies.through
+
+@admin.register(ScoringScheme)
+class ScoringScheme(admin.ModelAdmin):
+    pass
+
+@admin.register(Round)
+class Round(admin.ModelAdmin):
+    model = Round
+    inlines = [
+        MoviesInline,
+    ]
+
+@admin.register(RoundGuess)
+class RoundGuess(admin.ModelAdmin):
+    pass
