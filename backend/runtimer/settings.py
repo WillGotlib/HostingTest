add4cf8fbbd41911642aa914ddc378ee10af3b09
WillGotlib <37600983+WillGotlib@users.noreply.github.com>
2023-11-12 21:03:59 -0500

    change folder name for clarity

--- backend/runtimer/settings.py
+++ backend/runtimer/settings.py
@@ -126,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer', # FOR PRODUCTION!!! Disables browseable API which is nice to have most of the time...
+    )
+}
