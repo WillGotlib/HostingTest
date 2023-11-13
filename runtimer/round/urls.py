2d195f8069e51c51016d5548621993bac610c828
WillGotlib <37600983+WillGotlib@users.noreply.github.com>
2023-11-12 19:49:30 -0500

    added schemes list + generate views

--- runtimer/round/urls.py
+++ runtimer/round/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.mainpage, name='Main Game Page'),
-    
+    path('generate/', views.NewRoundView.as_view(), name='index'),
+    path('schemes/', views.SchemesView.as_view(), name='schemes'),
     path('schemes/load/', views.SchemesLoadUtilityView.as_view(), name='loadSchemes'),
     path('delete/', views.DeleteUtilityView.as_view(), name='delete'),
 ]
--- runtimer/round/views.py
+++ runtimer/round/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import Round, Movie, ScoringScheme
+from .utils import SCHEME_CHOICES, SCHEME_LIST, SCHEME_DESCRIPTIONS
 from django.http import FileResponse
 
-from rest_framework.views import APIView
+from rest_framework.views import APIView, ListAPIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.renderers import JSONRenderer
@@ -14,6 +15,27 @@ def mainpage(request):
     img = open('static/test/wonka-flash.jpg', 'rb')
     return FileResponse(img)
     # return Response({"Message": "This worked"}, status=203)
+
+class NewRoundView(APIView):
+    def post(self, request):
+        # form = RoundGenForm(request.data)
+        # if not form.is_valid():
+        #     return Response("FAILURE")
+        # print(form.cleaned_data)
+        sch = SCHEME_LIST[0] if request.data['scheme'] == "" else request.data['scheme']
+        print(sch)
+        round = Round.objects.create(size=int(request.data['size']), scheme=ScoringScheme.objects.get(pk=sch))
+        print("---- NEWROUNDVIEW: Creating New Round ----")
+        round.generate_movies(self.request)
+        ser = RoundSerializer(round)
+        return Response(ser.data)
+
+class SchemesView(ListAPIView):
+    def get(self, request):
+        d = {}
+        for sch in SCHEME_LIST:
+            d[sch] = {'name': SCHEME_CHOICES[sch], 'description': SCHEME_DESCRIPTIONS[sch]}
+        return Response({'list': SCHEME_LIST, 'dictionary': d})
 
 class SchemesLoadUtilityView(APIView):
     def get(self, request):
